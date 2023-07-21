dd441a06a0cde289d5e7dab3fbec210b51a641bc
kumar nishant <kumarnishu82@gmail.com>
2023-07-21 15:35:11 +0530

    added spring validation concept

---
+++ src/main/java/com/book/citi/SpringvalidationApplication.java
@@ -0,0 +1,14 @@
+package com.book.citi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringvalidationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringvalidationApplication.class, args);
+
+	}
+
+}
